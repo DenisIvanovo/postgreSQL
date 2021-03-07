76e06bd89661153be1d4cdd0b314d8f6e4b288af
DenisIvanovo <78604494+DenisIvanovo@users.noreply.github.com>
2021-03-07 13:41:31 +0300

    Add files via upload

---
+++ add_info_table.py
@@ -0,0 +1,31 @@
+"""
+Добавляем данные в таблицу.
+"""
+import psycopg2
+from psycopg2 import Error
+
+try:
+    # Подключаемся к существующей базе данных.
+    connection = psycopg2.connect(user='postgres',
+                                  password='fegBCXtf8',
+                                  host='localhost',
+                                  port='5432',
+                                  database='den_shop')
+    print('Мы удачно подключились к базе данных.')
+    cursor = connection.cursor()
+    for i in range(3):
+        insert_query = """ INSERT INTO Users ("user", "name", "phote", "email", "password")
+                                  VALUES ( 'd', 'denis','89997307022','md@mail.ru', 3432423)"""
+
+        # Выполнение команды: это создает новую таблицу
+        cursor.execute(insert_query)
+        connection.commit()
+        print("Данные внесены в базу")
+
+except (Exception, Error) as error:
+    print("Ошибка при работе с PostgreSQL", error)
+finally:
+    if connection:
+        cursor.close()
+        connection.close()
+        print("Соединение с PostgreSQL закрыто")
