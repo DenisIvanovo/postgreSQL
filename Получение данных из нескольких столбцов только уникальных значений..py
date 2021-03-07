6974f9cd1071fb5e2632306b151b5748d121dbab
DenisIvanovo <78604494+DenisIvanovo@users.noreply.github.com>
2021-03-07 16:16:57 +0300

    Add files via upload

---
+++ Получение данных из нескольких столбцов только уникальных значений..py
@@ -0,0 +1,32 @@
+"""
+Получение данных из нескольких столбцов только уникальных значений.
+"""
+import psycopg2
+from psycopg2 import Error
+
+try:
+    # Подключаемся к существующей базе данных.
+    connection = psycopg2.connect(user='postgres',
+                                  password='fegBCXtf8',
+                                  host='localhost',
+                                  port='5432',
+                                  database='den_shop')
+    print('Мы удачно подключились к базе данных.')
+    cursor = connection.cursor()
+
+    insert_query = """ SELECt DISTINCT nic, telephone, email from den"""
+
+    # Выполнение команды: получение информации из нескольких столбцов
+    cursor.execute(insert_query)
+    record = cursor.fetchall()
+    print('В цикле выходными данными являются полученные данные ')
+    for i in record:
+        print(i)
+
+except (Exception, Error) as error:
+    print("Ошибка при работе с PostgreSQL", error)
+finally:
+    if connection:
+        cursor.close()
+        connection.close()
+        print("Соединение с PostgreSQL закрыто")
