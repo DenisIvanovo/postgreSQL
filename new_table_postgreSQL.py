69c3e4173d42444d668366be2c382ea8670fcad1
DenisIvanovo <78604494+DenisIvanovo@users.noreply.github.com>
2021-03-08 19:17:38 +0300

    Add files via upload

--- new_table_postgreSQL.py
+++ new_table_postgreSQL.py
@@ -16,20 +16,15 @@ try:
     # Создаем курсор для выполнения операцийй с базой данных.
     cursor = connection.cursor()
     # Пишем запрос SQL
-    create_table_query = ''' create table data (
-	                                                    "id" bigserial PRIMARY KEY ,
-	                                                    "first_name" VARCHAR(50) NOT NULL ,
-	                                                    "last_name" VARCHAR(50) NOT NULL,
-	                                                    "phone" VARCHAR(20) NOT NULL,
-	                                                    "email" VARCHAR(50),
-	                                                    "gender" VARCHAR(6) NOT NULL,
-	                                                    "job" VARCHAR(50) NOT NULL,
-	                                                    "A_country" VARCHAR(50) NOT NULL,
-	                                                    "сity" VARCHAR(50) NOT NULL,
-	                                                    "Currency" VARCHAR(50)NOT NULL,
-	                                                    "money" VARCHAR(10) NOT NULL,
-	                                                    "date_of_birth" DATE,
-	                                                    "credit_card" VARCHAR(20) NOT NULL
+    create_table_query = ''' create table data_den (
+	                                                id bigserial PRIMARY KEY ,
+	                                                first_name VARCHAR(50) NOT NULL ,
+	                                                last_name VARCHAR(50) NOT NULL,
+	                                                phone VARCHAR(20) NOT NULL,
+	                                                email VARCHAR(50),
+	                                                gender VARCHAR(6) NOT NULL,
+	                                                job VARCHAR(50) NOT NULL,
+	                                                country VARCHAR(50) NOT NULL 
                                                         );'''
     # Выполнение команды: это создает новую таблицу
     cursor.execute(create_table_query)
