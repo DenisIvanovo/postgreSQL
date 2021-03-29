03394c4ff09a3aa33a1ad34cc874150f7054c497
DenisIvanovo <78604494+DenisIvanovo@users.noreply.github.com>
2021-03-29 21:00:31 +0300

    Add files via upload

---
+++ Выборка с определеной позиции.py
@@ -0,0 +1,47 @@
+"""
+Выборка записей с определеной позиции.
+"""
+
+
+import json
+import psycopg2
+from psycopg2 import Error
+
+
+# Параметры для подключения к базе данных.
+with open('info_db.json')as file:
+    data = json.load(file)
+
+# Подключаемся к существующей базе данных.
+try:
+    connection = psycopg2.connect(
+                                    user=data['user'],
+                                    password=data['password'],
+                                    port=data['port'],
+                                    host=data['host'],
+                                    database=data['database'])
+
+    print('Удачное подключение к базе данных.')
+    # Создаем курсор для работы с базой данных.
+    cursor = connection.cursor()
+    # Пишем команду на выполнение.
+    select_limit = """ SELECT * FROM data_shop OFFSET 50 limit 10
+                    """
+    # Выполнение команды.
+    cursor.execute(select_limit)
+    result = cursor.fetchall()
+    # Если результат пустой
+    if not result:
+        print('В базе данных таких записей обнаружено не было.')
+    else:
+        for i in result:
+            print(i)
+
+except (Exception, Error)as error:
+    print('Ошибка при подключению к базе данных.', error)
+
+finally:
+    if connection:
+        cursor.close()
+        connection.close()
+        print('Соединение с базой данных закрыто')
